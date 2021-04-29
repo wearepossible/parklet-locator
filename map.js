a773f55b34d76fee21a67672502b51fa32f11470
Duncan Geere <radio.edit@gmail.com>
2021-04-29 17:00:43 +0200

    Adding geolocator and map controls

--- map.js
+++ map.js
@@ -1,4 +1,3 @@
-
 // Access token
 mapboxgl.accessToken = 'pk.eyJ1Ijoid2VhcmVwb3NzaWJsZSIsImEiOiJja2tncjNkdGMxODJ3MnBxdWllMXhncDV2In0.ud6gHlMUoxsJS5yDPdXtaA';
 
@@ -7,5 +6,19 @@ var map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/wearepossible/ckmx9appa0lw117n2trmek7bq', // style URL
     center: [-0.12, 51.5], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    zoom: 12 // starting zoom
 });
+
+// Add navigation control
+map.addControl(new mapboxgl.NavigationControl());
+
+// Add geolocator
+// Add geolocate control to the map.
+map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true
+    })
+);
