f1c6b24984b9f3c625666e3329715c1cfe07fcc9
Duncan Geere <radio.edit@gmail.com>
2021-04-29 16:53:36 +0200

    New map, more buttons

--- map.js
+++ map.js
@@ -5,7 +5,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoid2VhcmVwb3NzaWJsZSIsImEiOiJja2tncjNkdGMxODJ3M
 // Set up the map
 var map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    style: 'mapbox://styles/wearepossible/ckmx9appa0lw117n2trmek7bq', // style URL
     center: [-0.12, 51.5], // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
